1dd2d9ea0b9b11c676c06c6a599a8ad6b4dfe5bc
Simone Basso <bassosimone@gmail.com>
2026-01-03 14:47:11 +0100

    feat: add tests for failed dials

---
+++ dialer_test.go
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package dnsoverstream
+
+import (
+	"context"
+	"crypto/tls"
+	"net"
+	"net/netip"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTCPStreamDialerDialContextCanceled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	dialer := &tcpStreamDialer{nd: &net.Dialer{}}
+	_, err := dialer.DialContext(ctx, netip.MustParseAddrPort("127.0.0.1:53"))
+	require.ErrorIs(t, err, context.Canceled)
+}
+
+func TestTLSStreamDialerDialContextCanceled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	dialer := &tlsStreamDialer{nd: &tls.Dialer{NetDialer: &net.Dialer{}, Config: &tls.Config{}}}
+	_, err := dialer.DialContext(ctx, netip.MustParseAddrPort("127.0.0.1:853"))
+	require.ErrorIs(t, err, context.Canceled)
+}
+
+func TestQUICStreamDialerDialContextCanceled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	lc := &net.ListenConfig{}
+	pconn, err := lc.ListenPacket(context.Background(), "udp", "127.0.0.1:0")
+	require.NoError(t, err)
+	defer pconn.Close()
+
+	dialer := &quicStreamDialer{
+		qd: NewQUICDialer(pconn, "example.com"),
+	}
+	_, err = dialer.DialContext(ctx, netip.MustParseAddrPort("127.0.0.1:853"))
+	require.ErrorIs(t, err, context.Canceled)
+}
